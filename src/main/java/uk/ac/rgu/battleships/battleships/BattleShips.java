c2cec90fcc57644dec597ae1b262cbb037266e30
euan <euanchree@gmail.com>
2022-03-26 17:52:26 +0000

    Changed println.

--- src/main/java/uk/ac/rgu/battleships/battleships/BattleShips.java
+++ src/main/java/uk/ac/rgu/battleships/battleships/BattleShips.java
@@ -12,6 +12,6 @@ package uk.ac.rgu.battleships.battleships;
 public class BattleShips {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Starting program!");
     }
 }
