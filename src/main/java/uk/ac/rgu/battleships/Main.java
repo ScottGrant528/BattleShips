6274843d8d1d25c54ac3a2c0d14cdf150d92a207
ScottGrant528 <56115663+ScottGrant528@users.noreply.github.com>
2022-03-26 21:53:09 +0000

    big brain

--- src/main/java/uk/ac/rgu/battleships/Main.java
+++ src/main/java/uk/ac/rgu/battleships/Main.java
@@ -14,8 +14,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("----Starting program!----");
 
-        Peer p = new Peer();
-        p.newPeer();
     
     }
 }
