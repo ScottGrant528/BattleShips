7225eace8cbc042b56ea38d838d0ddc7fe47726b
euan <euanchree@gmail.com>
2022-03-26 22:25:47 +0000

    Created the game board object

---
+++ src/main/java/uk/ac/rgu/battleships/GameBoard.java
@@ -0,0 +1,11 @@
+package uk.ac.rgu.battleships;
+
+public class GameBoard {
+    // 2D Array to hold
+    String[][] board;
+
+    // Constructor
+    public GameBoard(String[][] givenBoard) {
+        this.board = givenBoard;
+    }
+}
--- src/main/java/uk/ac/rgu/battleships/Main.java
+++ src/main/java/uk/ac/rgu/battleships/Main.java
@@ -1,19 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package uk.ac.rgu.battleships;
-
 /**
  *
  * @author euan & scott
  */
 public class Main {
-
     public static void main(String[] args) throws Exception {
         System.out.println("----Starting program!----");
-
-    
     }
 }
